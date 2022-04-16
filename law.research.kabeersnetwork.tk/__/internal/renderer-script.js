0d2ce134f2d5668f7685975bc70039ae5f72c09a
Kabeer Jaffri <kabeer11000@gmail.com>
2022-04-16 18:41:30 +0500

    Update renderer-script.js

--- law.research.kabeersnetwork.tk/__/internal/renderer-script.js
+++ law.research.kabeersnetwork.tk/__/internal/renderer-script.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", async () => {
+// document.addEventListener("DOMContentLoaded", async () => {
     const url = "./markdown.md";
     document.querySelector("content").innerHTML = marked.parse(await (await fetch(url)).text());
     await window.__kn___internal_renderComplete();
-});
+// });
+// 
