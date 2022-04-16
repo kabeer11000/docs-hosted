4d2bd57aaf65e25b35e12303591da4e890ea21b2
Kabeer Jaffri <kabeer11000@gmail.com>
2022-04-16 18:32:49 +0500

    Create asset-urls.js

---
+++ law.research.kabeersnetwork.tk/__/internal/asset-urls.js
@@ -0,0 +1,31 @@
+window["__kn.website.static.research.blog:renderer.assetURLS"] = [{
+    type: "link",
+    attach: "head",
+    position: "bottom",
+    optionalProps: {href: "/__/internal/blog.markdown.css"}
+}, {
+    type: "link",
+    attach: "head",
+    position: "bottom",
+    optionalProps: {href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"}
+}, {
+    type: "script",
+    attach: "body",
+    position: "bottom",
+    optionalProps: {src: "https://cdn.jsdelivr.net/gh/kabeer11000/docs-hosted@research.kabeersnetwork.tk/_internal/postMessage.js"}
+}, {
+    type: "script",
+    attach: "body",
+    position: "bottom",
+    optionalProps: {src: "./__/internal/globals.js"}
+}, {
+    type: "script",
+    attach: "body",
+    position: "bottom",
+    optionalProps: {defer: true, src: "https://cdn.jsdelivr.net/npm/marked/marked.min.js"}
+}, {
+    type: "script",
+    attach: "body",
+    position: "bottom",
+    optionalProps: {defer: true, src: "./__/internal/renderer-script.js"}
+}]
